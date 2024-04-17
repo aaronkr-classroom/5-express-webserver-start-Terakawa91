c3992866c492b4930b4ba054fcdebf1f12e05eb5
Terakawa91 <68772480+Terakawa91@users.noreply.github.com>
2024-04-17 04:56:47 +0000

    lesson-11_fin

--- lesson-11/controllers/homeController.js
+++ lesson-11/controllers/homeController.js
@@ -4,14 +4,34 @@
 /**
  * Listing 10.2
  */
-exports.respondWithName = (req, res) => {
+exports.respondWithName1 = (req, res) => {
     res.render('index');
 }
 
 /**
  * Listing 10.3
  */
-exports.respondWithName2 = (req, res) => {
-    let paramsName = req.params.myName;
-    res.render('index', { name: paramsName });
+exports.getHome = (req, res) => { res.render('index'); } // "/" index
+// exports.getContact = (req, res) => {} // "/contact" GET index
+// exports.postContact = (req, res) => {} // "/contact" POST index
+
+exports.respondWithName = (req, res) => {
+    let paramsName = req.params.myName; // 라우트의 매개변수 /name/수:myName
+    res.render('index', { 
+        name: paramsName,
+        people: {
+            usa: {
+                einstein: "Albert Einstein",
+                musk: "Elon Musk",
+                jobs: "Steve Jobs"
+            },
+            korea: {
+                psy: "싸이다!",
+                kim: "김민수",
+            },
+            mexico: {
+                jesus: "Jesus Alvarez"
+            }
+        }
+    });
 };
--- lesson-11/main.js
+++ lesson-11/main.js
@@ -2,3 +2,35 @@
 "use strict";
 
 // @TODO: 웹 서버를 만들고 실행한다.
+// BEST 습관 : 먼저 모든 상수나 변수 정의
+
+const port = 3000,
+    express = require('express'),
+    layouts = require('express-ejs-layouts'),
+    homeController = require('./controllers/homeController'),
+    errorController = require('./controllers/errorController'),
+    app = express();
+
+// BEST 습관 : 2단계 앱 설정 (set 함수, 그다음에 use 함수)
+app.set("port", process.env.PORT || port);
+app.set("view engine", "ejs");
+
+app.use(layouts);
+app.use(express.static("public"));
+
+// Aaron's 습관 : GET 라우트 먼저 그 다음 POST
+app.get("/", homeController.getHome);
+app.get("/name/:myName", homeController.respondWithName);
+app.get("/test", (req, res) => {
+    res.sendFile("./public/test.html");
+});
+
+// errorController app.use()
+app.use(errorController.logErrors);
+app.use(errorController.resNotFound);
+app.use(errorController.resInternalError);
+
+// BEST 습관 : 마지막 listen 함수
+app.listen(app.get("port"), () => {
+    console.log(`Server at: http://localhost:${app.get("port")}`);
+});
